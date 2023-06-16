41a5329eb85902a4ffdace06f353b746aa09c7f2
Sanjin888 <sanjinkurtalija@gmail.com>
2023-06-16 02:00:34 +0200

    adding firebase authentication

---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider} from "firebase/auth"; 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCm54YGIP2m8EcF_r2VSJlYr9sQ78x-6ZY",
+  authDomain: "social-media-17646.firebaseapp.com",
+  projectId: "social-media-17646",
+  storageBucket: "social-media-17646.appspot.com",
+  messagingSenderId: "302847398285",
+  appId: "1:302847398285:web:e166690ad970b9af45adc4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
---
+++ src/pages/login.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+
+export const Login = () => {
+
+  const navigate = useNavigate();
+
+
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+    navigate("/");
+  };
+  return (
+    <div>
+      <p>Sign In With Google To Continue</p>
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+    </div>
+  );
+};

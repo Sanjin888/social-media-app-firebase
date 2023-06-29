f30c6639855962ee507f91610fbbadbd55b9aefb
Sanjin888 <sanjinkurtalija@gmail.com>
2023-06-29 16:26:13 +0200

    adding collection

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -4,6 +4,7 @@ import {yupResolver} from "@hookform/resolvers/yup";
 import { addDoc, collection } from "firebase/firestore";
 import { auth, db } from "../../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import {useNavigate} from "react-router-dom";
 
 interface CreateFormData{
   title:string;
@@ -13,6 +14,7 @@ interface CreateFormData{
 export const CreateForm = () => {
 
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   const schema = yup.object().shape({
     title: yup.string().required("You must add a title"),
@@ -31,6 +33,8 @@ export const CreateForm = () => {
       username: user?.displayName,
       userId: user?.uid, 
     })
+
+    navigate("/");
   }
   return (
     <div>
@@ -39,7 +43,7 @@ export const CreateForm = () => {
         <p style={{color: "red"}} >{errors.title?.message}</p>
         <textarea placeholder="Description.." {...register("description")} />
         <p style={{color: "red"}}>{errors.description?.message}</p>
-        <input type="submit"/>
+        <input type="submit" className="submitFrom"/>
       </form>
     </div>
   );
